6c08be50c39d8544513a2d131295dcc3b3a45bd5
Marcus Grönnå <mgronna@gmail.com>
2025-12-11 10:40:48 +0100

    feat: add route for ProgramDetailPage to handle program-specific views

---
+++ frontend/src/routes/programs/$programId.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { ProgramDetailPage } from '../../pages/ProgramDetailPage'
+
+export const Route = createFileRoute('/programs/$programId')({
+  component: ProgramDetailPage,
+})
