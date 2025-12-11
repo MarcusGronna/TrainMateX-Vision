e0066b7569646163f7cffd2b59d2c6d4dd2c042b
Marcus Grönnå <mgronna@gmail.com>
2025-12-11 16:46:26 +0100

    feat: add Workout entity and establish relationship with TrainingProgram

--- backend/TrainMateX.Domain/Entities/TrainingProgram.cs
+++ backend/TrainMateX.Domain/Entities/TrainingProgram.cs
@@ -3,10 +3,15 @@ using TrainMateX.Domain.Entities;
 public class TrainingProgram
 {
     public Guid Id { get; set; }
+
+    // FK
     public Guid UserProfileId { get; set; }
     public string Name { get; set; } = null!;
     public string? Description { get; set; }
     public string? Level { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    // Navigation
     public UserProfile UserProfile { get; set; } = null!;
+    public ICollection<Workout> Workouts { get; set; } = new List<Workout>();
 }
