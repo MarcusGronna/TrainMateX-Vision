ac2196e4a9e1e4a92d2b1722c0b39ff8b03d9546
Marcus Grönnå <mgronna@gmail.com>
2025-12-12 09:41:30 +0100

    refactor: remove unused using directives in TrainingProgramService

--- backend/TrainMateX.Application/Services/TrainingProgramService.cs
+++ backend/TrainMateX.Application/Services/TrainingProgramService.cs
@@ -1,6 +1,3 @@
-using System.ComponentModel;
-using System.Security.Cryptography.X509Certificates;
-
 public class TrainingProgramService
 {
     private readonly ITrainingProgramRepository _repository;
