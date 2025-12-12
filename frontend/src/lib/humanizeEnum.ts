cc44835c82364f4acfcb7b60353f7b9bfc75c678
Marcus Grönnå <mgronna@gmail.com>
2025-12-12 16:39:22 +0100

    feat: update humanizeEnum function to handle undefined and null values

--- frontend/src/lib/humanizeEnum.ts
+++ frontend/src/lib/humanizeEnum.ts
@@ -1,4 +1,7 @@
-export function humanizeEnum(value: string): string {
+export function humanizeEnum(value: string | undefined | null): string {
+  // Return empty string if value is falsy
+  if (!value || typeof value !== 'string') return ''
+
   // Ex: "BodyWeight" -> "Body Weight", "PullUpBar" -> "Pull Up Bar"
   return value.replace(/([a-z0-9])([A-Z])/g, '$1 $2')
 }
